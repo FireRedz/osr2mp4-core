894a912eb3c5de9b3a75ed1d62d9da76b3448f86
FireRedz <fireredz69420@gmail.com>
2021-05-11 10:13:18 +0800

    disable broken ass smokes

--- osr2mp4/ImageProcess/Objects/Components/Smokes.py
+++ osr2mp4/ImageProcess/Objects/Components/Smokes.py
@@ -3,33 +3,41 @@
 	Certified bruh moment code, yuitora pls help, i cant math man.
 
 """
-
+import numpy as np
 from PIL import Image
 
 from osr2mp4.ImageProcess import imageproc
 from osr2mp4.ImageProcess.PrepareFrames.YImage import YImage
 from osr2mp4.ImageProcess.Animation.alpha import fade
+from osr2mp4.ImageProcess.Animation import easings
 
 class Smoke:
 	def __init__(self, settings: object):
 		self.settings = settings
-		self.frames = [YImage('cursor-smoke', settings).img]
-		self.frames = [self.frames[0], *fade(self.frames[0], 1, 0, 1000, settings)]
+		self.frames = [YImage('cursor-smoke', settings, 0.5).img]
+		self.frames = [self.frames[0], *fade(self.frames[0], 1, 0, 1000, settings, easings.easeInQuad)]
 		self.states = {}
 
+
 	def add_to_frame(self, background: Image.Image, replay_info: object):
+		return # disable smokes for now
+
 		cx, cy = replay_info.x, replay_info.y
 		key = replay_info.keys_pressed
 
 		cx = int(cx * self.settings.playfieldscale) + self.settings.moveright
 		cy = int(cy * self.settings.playfieldscale) + self.settings.movedown
 
-		##  if key is touching the meme smoke button
+		## if key is touching the meme smoke button
 		if key & 16:
 			self.states[replay_info.time] = (cx, cy)
 
 
-		before = None
+		before = [cx, cy, replay_info.time]
+
+		# reduce
+		self.states = {time: pos for time, pos in self.states.items() if time > replay_info.time - 9000}
+
 		for time, pos in self.states.items():
 			if time > replay_info.time - 9000: # not sure what the actual duration is, should be around ~8-10s
 				
@@ -40,13 +48,17 @@ class Smoke:
 					frame = self.frames[0]
 
 				smoke_size = frame.size
+				duration = max(time - before[2], 1)
+				for time in np.arange(0, duration, self.settings.timeframe/self.settings.fps): # make inbetween (?)
+					x = easings.easeLinear(time, before[0], pos[0]-before[0], duration)
+					y = easings.easeLinear(time, before[1], pos[1]-before[1], duration)
 
-				x = int((pos[0] - smoke_size[0]/2))
-				y = int((pos[1] - smoke_size[1]/2))
+					x = int((x - smoke_size[0]/2))
+					y = int((y - smoke_size[1]/2))
+					imageproc.add(frame, background, x, y, topleft=True)
+
+				before = [*pos, time]
 
 
-				imageproc.add(frame, background, x, y, topleft=True)
-
-				before = pos
 			
 		
