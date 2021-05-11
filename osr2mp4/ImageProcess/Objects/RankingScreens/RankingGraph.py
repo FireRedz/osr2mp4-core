afdef925f358d8877892741f4353c4b4c86a4e18
FireRedz <fireredz69420@gmail.com>
2021-05-11 14:46:06 +0800

    hp graph thing

--- osr2mp4/ImageProcess/Objects/RankingScreens/RankingGraph.py
+++ osr2mp4/ImageProcess/Objects/RankingScreens/RankingGraph.py
@@ -36,6 +36,7 @@ class RankingGraph(ARankingScreen):
 		# how the fuck do i change the matplotlib resolution
 		# the way i did it is just force resize it to the graph box size
 		# which make the image quality terrible
+		# TODO: make the thing not look terrible
 		life = self.replay_info.life_bar_graph.split('|')
 		life = [[float(_) for _ in e.split(',')] for e in life if len(e.split(',')) > 1 and e.split(',')[1]] # remove null shit
 		life.sort(key=lambda x: x[1])
