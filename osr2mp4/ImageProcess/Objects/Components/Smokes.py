6fa82392be8a914ba73b91e9bd11009050aaf86a
FireRedz <fireredz69420@gmail.com>
2021-05-28 13:07:12 +0800

    smokes

--- osr2mp4/ImageProcess/Objects/Components/Smokes.py
+++ osr2mp4/ImageProcess/Objects/Components/Smokes.py
@@ -3,23 +3,68 @@
 	Certified bruh moment code, yuitora pls help, i cant math man.
 
 """
+
+import math
 import numpy as np
 from PIL import Image
+from recordclass import recordclass
 
 from osr2mp4.ImageProcess import imageproc
 from osr2mp4.ImageProcess.PrepareFrames.YImage import YImage
 from osr2mp4.ImageProcess.Animation.alpha import fade
 from osr2mp4.ImageProcess.Animation import easings
 
+SmokeState = recordclass('SmokeState', 'x y time endtime keys is_fadeout fadeout_frame')
+scaling = 0.625 # ?
+
+
 class Smoke:
 	def __init__(self, settings: object):
 		self.settings = settings
-		self.frames = [YImage('cursor-smoke', settings, 0.5).img]
-		self.frames = [self.frames[0], *fade(self.frames[0], 1, 0, 1000, settings, easings.easeInQuad)]
+		self.smoke = YImage('cursor-smoke', settings, 0.5).img
+		self.smoke_fadeout = [*fade(self.smoke, 0.6, 0, 4000, settings, easings.easeInQuad)]
 		self.states = {}
+		self.last_state = None
 
 
-	def add_to_frame(self, background: Image.Image, replay_info: object):
+	def add_to_frame(self, background: Image, replay_info: object):
+		cx, cy, key = (replay_info.x, replay_info.y, 
+						replay_info.keys_pressed)
+
+		cx = int(cx * self.settings.playfieldscale) + self.settings.moveright
+		cy = int(cy * self.settings.playfieldscale) + self.settings.movedown
+
+		if key & 16: # if smoking
+			self.states[replay_info.time] = SmokeState(cx, cy, replay_info.time, replay_info.time + 4000, replay_info.keys_pressed, False, 0)
+
+		last_state = None
+		for _, smoke in self.states.items():
+			if replay_info.time < smoke.endtime:
+				if smoke.fadeout_frame >= len(self.smoke_fadeout):
+					continue
+
+				if not last_state:
+					last_state = smoke
+
+				imageproc.add(self.smoke_fadeout[smoke.fadeout_frame], background, smoke.x, smoke.y)
+
+				# make retarded inbetween
+				duration = max(smoke.time - last_state.time, 1)
+				for time in np.arange(0, duration, self.smoke.size[0]):
+					x = easings.easeLinear(time, last_state.x, smoke.x-last_state.x, duration)
+					y = easings.easeLinear(time, last_state.y, smoke.y-last_state.y, duration)
+
+					imageproc.add(self.smoke_fadeout[smoke.fadeout_frame], background, x, y)
+
+				smoke.fadeout_frame += 1
+				last_state = smoke
+		
+		
+		
+
+
+
+	def retarded_old_add_to_frame(self, background: Image.Image, replay_info: object):
 		return # disable smokes for now
 
 		cx, cy = replay_info.x, replay_info.y
